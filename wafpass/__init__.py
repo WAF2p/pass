57f5227fd18656a84d441b8bff35a8ec6929bb28
Sascha Lewandowski <s.lewandowski@it-lew.de>
2026-03-22 12:01:49 +0100

    fix release workflow

--- wafpass/__init__.py
+++ wafpass/__init__.py
@@ -1,2 +1,8 @@
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version as _pkg_version
+
+try:
+    __version__ = _pkg_version("wafpass")
+except PackageNotFoundError:
+    __version__ = "0.1.0-dev"
+
 __name_full__ = "WAF++ PASS"
